f085146f5efbe59433a4c4e31502de54a0b48dde
AnanthatAWS <ananva@amazon.com>
2018-02-13 22:57:24 -0800

    Update linux_ssh_port_check.rb

--- Compliance/InSpec/PortCheck/controls/linux_ssh_port_check.rb
+++ Compliance/InSpec/PortCheck/controls/linux_ssh_port_check.rb
@@ -1,8 +1,13 @@
-require 'rbconfig'
-is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-if ! is_windows
-  describe port(22) do
-    it { should be_listening }
-    its('addresses') {should_not include '0.0.0.0'}
+control 'Linux instance check' do
+  title 'SSH access'
+  desc 'SSH port should not be open to the world'
+  impact 0.9
+  require 'rbconfig'
+  is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+  if ! is_windows
+    describe port(22) do
+      it { should be_listening }
+      its('addresses') {should_not include '0.0.0.0'}
+    end
   end
 end
