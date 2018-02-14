aaa33160b3416e6bd44fdb400ab8cf35ac5a4914
AnanthatAWS <ananva@amazon.com>
2018-02-13 22:57:52 -0800

    Update windows_rdp_port_check.rb

--- Compliance/InSpec/PortCheck/controls/windows_rdp_port_check.rb
+++ Compliance/InSpec/PortCheck/controls/windows_rdp_port_check.rb
@@ -1,8 +1,13 @@
-require 'rbconfig'
-is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-if is_windows
-  describe port(3389) do
-    it { should be_listening }
-    its('addresses') {should_not include '0.0.0.0'}
+control 'Windows instance check' do
+  title 'RDP access'
+  desc 'RDP port should not be open to the world'
+  impact 0.9
+  require 'rbconfig'
+  is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+  if is_windows
+    describe port(3389) do
+      it { should be_listening }
+      its('addresses') {should_not include '0.0.0.0'}
+    end
   end
 end
